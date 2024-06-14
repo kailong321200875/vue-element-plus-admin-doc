8dfcda6cbe9db33dbacff4ffbca5f3cc773f9c4d
kailong321200875 <kailong321200875@users.noreply.github.com>
2024-06-14 08:27:52 +0000

    deploy: af3dbffc9762aed9722ea1f7feee705d3f725aed

---
+++ assets/README.md.b4d8392e.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as n,a}from"./app.9e1f3eef.js";const s='{"title":"vue-element-plus-admin-doc","description":"","frontmatter":{},"headers":[{"level":2,"title":"如何本地开发","slug":"如何本地开发"}],"relativePath":"README.md","lastUpdated":1718353647564}',t={},o=a('',3);t.render=function(a,s,t,l,d,c){return e(),n("div",null,[o])};export default t;export{s as __pageData};
---
+++ assets/components_footer.md.e0d0d468.lean.js
@@ -0,0 +1,1 @@
+import{o as a,c as n,a as s}from"./app.9e1f3eef.js";const t='{"title":"Footer 页脚","description":"","frontmatter":{},"headers":[{"level":2,"title":"用法","slug":"用法"}],"relativePath":"components/footer.md","lastUpdated":1718353647564}',e={},p=s('',5);e.render=function(s,t,e,o,c,l){return a(),n("div",null,[p])};export default e;export{t as __pageData};
